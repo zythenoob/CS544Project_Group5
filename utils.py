ee803a89a767a98e995eb348b23d936bc84b5d98
zythenoob <jiayezhu@usc.edu>
2022-11-02 18:05:54 -0700

    masked prediection for til

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from typing import Dict
 
+import torch
+
 
 def batch_to_device(batch, device):
     if isinstance(batch, Dict):
@@ -8,3 +10,9 @@ def batch_to_device(batch, device):
     else:
         raise ValueError('Batch type not supported')
     return batch
+
+
+def mask_logits_class(logits, label_offset, task):
+    mask = torch.zeros_like(logits)
+    mask[:, label_offset[task]:label_offset[task + 1]] = 1
+    return logits * mask
