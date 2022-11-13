reduce computation & bug fix

model/base.py
@@ -23,17 +23,16 @@ class CLModel(nn.Module):
     def end_task(self, dl, offset):
         pass
 
-    def forward(self, x, y, attn_mask):
-        output = self.backbone(input_ids=x, labels=y, attention_mask=attn_mask, output_hidden_states=True)
-        loss = output.loss
+    def forward(self, x, attn_mask=None):
+        output = self.backbone(input_ids=x, attention_mask=attn_mask, output_hidden_states=True)
         logits = output.logits
         features = output.hidden_states
-        return logits, features, loss
+        return logits, features
 
     @torch.no_grad()
-    def get_preds(self, x, y, attn_mask):
+    def get_preds(self, x, attn_mask):
         assert not self.training
-        logits, _, _ = self.forward(x=x, y=y, attn_mask=attn_mask)
+        logits, _ = self.forward(x=x, attn_mask=attn_mask)
         return logits
 
 

model/derpp.py
@@ -20,20 +20,22 @@ class DERPP(CLModel):
         self.beta = 1.0
 
     def observe(self, x, y, attn_mask):
-        logits, _, loss = self.forward(x, y, attn_mask)
+        logits, _ = self.forward(x, attn_mask)
+        loss = F.cross_entropy(logits, y)
+        aux_loss = 0
         if not self.buffer.is_empty():
-            aux_loss = 0
             # knowledge distillation
             buffer_x, buffer_y, buffer_logits, buffer_attn_mask = self.buffer.get_data(self.replay_size, self.device)
-            _, _, buffer_loss = self.forward(buffer_x, buffer_y, buffer_attn_mask)
+            s_logits, _ = self.forward(buffer_x, attn_mask=buffer_attn_mask)
+            buffer_loss = F.cross_entropy(s_logits, buffer_y)
             aux_loss += self.alpha * buffer_loss
             # er
             buffer_x, buffer_y, buffer_logits, buffer_attn_mask = self.buffer.get_data(self.replay_size, self.device)
-            s_logits, _, _ = self.forward(buffer_x, buffer_y, buffer_attn_mask)
+            s_logits, _ = self.forward(buffer_x, attn_mask=buffer_attn_mask)
             aux_loss += self.beta * F.mse_loss(buffer_logits, s_logits)
-            loss = loss + aux_loss
+        loss = loss + aux_loss
 
-        self.buffer.add_data(x=x, y=y, logits=logits, attn_mask=attn_mask)
+        self.buffer.add_data(x=x, y=y, logits=logits.detach(), attn_mask=attn_mask)
         return logits, loss
 
     def end_task(self, dataloader, label_offset):

model/er.py
@@ -18,10 +18,12 @@ class ER(CLModel):
         self.replay_size = config.batch_size
 
     def observe(self, x, y, attn_mask):
-        logits, _, loss = self.forward(x, y, attn_mask)
+        logits, _ = self.forward(x, attn_mask)
+        loss = F.cross_entropy(logits, y)
         if not self.buffer.is_empty():
             buffer_x, buffer_y, buffer_attn_mask = self.buffer.get_data(self.replay_size, self.device)
-            _, _, aux_loss = self.forward(buffer_x, buffer_y, buffer_attn_mask)
+            s_logits, _ = self.forward(buffer_x, attn_mask=buffer_attn_mask)
+            aux_loss = F.cross_entropy(s_logits, buffer_y)
             loss = loss + aux_loss
         self.buffer.add_data(x=x, y=y, attn_mask=attn_mask)
         return logits, loss

model/ewc.py
@@ -25,7 +25,8 @@ class EWC(CLModel):
         self._precision_matrices = None
 
     def observe(self, x, y, attn_mask):
-        logits, _, loss = self.forward(x, y, attn_mask)
+        logits, _ = self.forward(x, attn_mask)
+        loss = F.cross_entropy(logits, y)
         penalty = self.penalty()
         loss = loss + penalty
         return logits, loss
@@ -57,9 +58,8 @@ class EWC(CLModel):
         for batch in dataloader:
             self.backbone.zero_grad()
             batch = batch_to_device(batch, self.device)
-            x, y, attn_mask = batch['input_ids'], batch['labels'], batch['attention_mask']
-            y = y.long() + label_offset
-            output, _, _ = self.forward(x, y, attn_mask)
+            x, attn_mask = batch['input_ids'], batch['attention_mask']
+            output, _ = self.forward(x, attn_mask=attn_mask)
 
             label = output.max(1)[1].view(-1)
             loss = F.nll_loss(F.log_softmax(output, dim=1), label)